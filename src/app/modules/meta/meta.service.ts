869cd97790b43ff2cac3b6847569cffb71d1e79f
Israfil Hossen <sisrafilss@gmail.com>
2026-06-18 20:33:37 +0600

    65-5

--- src/app/modules/meta/meta.service.ts
+++ src/app/modules/meta/meta.service.ts
@@ -8,7 +8,7 @@ const fetchedDashboardMetaData = async (user: IJwtPayload) => {
   let metadata;
   switch (user.role) {
     case UserRole.ADMIN:
-      metadata = "Admin Meta Data";
+      metadata = await getAdminMetaData();
       break;
     case UserRole.DOCTOR:
       metadata = "Doctor meta data";
@@ -38,6 +38,47 @@ const getAdminMetaData = async () => {
       status: PaymentStatus.PAID,
     },
   });
+
+  const barChartData = await getBarChartData();
+  const piChartData = await getPiChartData();
+
+  return {
+    patientCount,
+    doctorCount,
+    adminCount,
+    appointmentCount,
+    paymentCount,
+    totalRevenue,
+    barChartData,
+    piChartData,
+  };
+};
+
+const getBarChartData = async () => {
+  const appointmentCountPerMonth = await prisma.$queryRaw`
+    SELECT DATE_TRUNC('month', "createdAt") AS month,
+    CAST(COUNT(*) AS INTEGER) AS count
+    FROM "appointments"
+    GROUP BY month
+    ORDER BY month ASC
+  `;
+
+  return appointmentCountPerMonth;
+};
+
+const getPiChartData = async () => {
+  const appointmentStatusDistribution = await prisma.appointment.groupBy({
+    by: ["status"],
+    _count: { id: true },
+  });
+
+  const formatedAppointmentStatusDistribution =
+    appointmentStatusDistribution.map(({ status, _count }) => ({
+      status,
+      count: Number(_count.id),
+    }));
+
+  return formatedAppointmentStatusDistribution;
 };
 
 export const MetaService = {
